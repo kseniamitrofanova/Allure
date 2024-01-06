d7fc2e9123d272b486bed8be4b7e600db0a378d9
KMitrofanova <vip.ksundra@mail.ru>
2024-01-07 00:39:11 +0300

    added SelenideTest

---
+++ src/test/java/ru/vipksu/SelenideTest.java
@@ -0,0 +1,27 @@
+package ru.vipksu;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideTest {
+    @Test
+    public void testIssueSearch(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+        $(".search-input").click();
+        $("#query-builder-test").sendKeys("kseniamitrofanova/");
+        $(".QueryBuilder-InputWrapper").submit();
+
+        $(linkText("kseniamitrofanova/MyProject")).click();
+        $("#issues-tab").click();
+        $(withText("#3")).should(Condition.exist);
+    }
+}
